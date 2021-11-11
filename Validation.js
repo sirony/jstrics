a1e30c1c1e122ebb2717c562477f9939f368d8dd
Md. Shariful Islam <shariful_25@yahoo.com>
2021-11-11 12:41:18 +0600

    Update Validation.js
    Commit for checkbox group function

--- Validation.js
+++ Validation.js
@@ -1,4 +1,26 @@
 /**
+ * This function is used for making group checkbox as rario behaviour 
+ * @param container (string)  - the id of checkboxes container
+ * @param el (dom) - current clicked checkbox
+ * @param required (bool) - status the option is required or not. By default false (not required) 
+ */
+
+function checkBoxGroup(container, el, required = false){
+
+  if(required){
+    $("#"+container).find("input[type='checkbox']").prop('checked', false);
+
+    if($(el).prop("checked") == true){
+        $(el).prop("checked", false);
+    }else{
+        $(el).prop("checked", true);
+    }
+   }else{
+        $(el).closest(".form-group").siblings().find("input[type='checkbox']").prop('checked', false);
+   }
+}
+
+/**
  * This function is used for limit length of character in the form input field
  * @param el - the element of the form like (input, textarea etc.)
  * @param length - the maximum lenght of the form element
